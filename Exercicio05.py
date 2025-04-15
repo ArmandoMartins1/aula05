916f63f0764833f59ef631e7e468e9e036c05ad1
Armando Martins <armando.amds97@gmail.com>
2025-04-15 09:49:33 -0300

    divisao aprimorada, tratamento de error para divisão por 0

---
+++ Exercicio05.py
@@ -0,0 +1,8 @@
+num1 = int(input("digite um valor: "))
+num2 = int(input("digite um valor: "))
+
+while num2 == 0:
+    num2 = int(input("Digite outro valor diferente de 0: "))
+
+div = num1/num2
+print(div)
